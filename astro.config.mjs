cd406dd2380669fae0b4312104ddff352a37a4f2
Vijay Pemmaraju <vijay@grooveapp.com>
2024-10-15 13:19:18 -0700

    Deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://us-election-comparisons.github.io',
   integrations: [react(), tailwind()]
 });
