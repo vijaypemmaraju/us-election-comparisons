ba511f74f287a2c05a8d4c746ace647d9e7d05cd
Vijay Pemmaraju <vijay@grooveapp.com>
2024-10-15 13:23:20 -0700

    FIx config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://us-election-comparisons.github.io',
+  site: 'https://vijaypemmaraju.github.io',
+  base: '/us-election-comparisons/',
   integrations: [react(), tailwind()]
 });
